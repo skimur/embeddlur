1d010f3ceb1681018767112300d7a3c6dac67435
Paul Knopf <theonlylawislove@gmail.com>
2015-11-16 02:04:52 -0500

    Added some bundle transformers.

--- src/Embedlur.Web.Public/App_Start/BundleConfig.cs
+++ src/Embedlur.Web.Public/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Optimization;
+using BundleTransformer.Core.Transformers;
 
 namespace Embedlur.Web.Public
 {
@@ -8,13 +9,17 @@ namespace Embedlur.Web.Public
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/js/photoswipe").Include(
-                 "~/Scripts/photoswipe.js",
-                 "~/Scripts/photoswipe-ui-default.js"));
-
-            bundles.Add(new StyleBundle("~/css/photoswipe").Include(
-                 "~/Content/photoswipe/photoswipe.css",
-                 "~/Content/photoswipe/default-skin/default-skin.css"));
+            var photoswipeScriptsBundle = new ScriptBundle("~/js/photoswipe").Include(
+                "~/Scripts/photoswipe.js",
+                "~/Scripts/photoswipe-ui-default.js");
+            photoswipeScriptsBundle.Transforms.Add(new ScriptTransformer());
+            bundles.Add(photoswipeScriptsBundle);
+            
+            var photoswipeStylesBundle = new StyleBundle("~/css/photoswipe").Include(
+                "~/Content/photoswipe/photoswipe.css",
+                "~/Content/photoswipe/default-skin/default-skin.css");
+            photoswipeStylesBundle.Transforms.Add(new StyleTransformer());
+            bundles.Add(photoswipeStylesBundle);
         }
     }
 }
